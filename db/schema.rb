d674a8a33828e3ae73a67112f87769f13e0c01c1
maekawa <kazumaekawa7218@gmail.com>
2020-10-02 12:13:38 +0900

    usersテーブルへのカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,12 @@
 ActiveRecord::Schema.define(version: 2020_10_02_022237) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_read", null: false
+    t.string "first_name_read", null: false
+    t.date "birth_date", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
