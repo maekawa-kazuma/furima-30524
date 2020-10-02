6c912b88253111743f65e04f65751a5b1356232b
maekawa <kazumaekawa7218@gmail.com>
2020-10-02 16:35:51 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+         
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/
+  VALID_NAME_READ_REGEX = /\A[ァ-ヶー－]+\z/
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: {with: VALID_NAME_REGEX}
+  validates :first_name, presence: true, format: {with: VALID_NAME_REGEX}
+  validates :last_name_read, presence: true, format: {with: VALID_NAME_READ_REGEX}
+  validates :first_name_read, presence: true, format: {with: VALID_NAME_READ_REGEX}
+  validates :birth_date, presence: true
+  validates :email, uniqueness: true
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :password_confirm, presence: true
 end
