2ca9307dbba78e9cbae24ceaef05cca1a3f28d6f
maekawa <kazumaekawa7218@gmail.com>
2020-10-03 11:42:30 +0900

    Userモデルのリファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,15 @@ class User < ApplicationRecord
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
   VALID_NAME_READ_REGEX = /\A[ァ-ヶー－]+\z/.freeze
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX }
-  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX }
-  validates :last_name_read, presence: true, format: { with: VALID_NAME_READ_REGEX }
-  validates :first_name_read, presence: true, format: { with: VALID_NAME_READ_REGEX }
-  validates :birth_date, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name, format: { with: VALID_NAME_REGEX }
+    validates :first_name, format: { with: VALID_NAME_REGEX }
+    validates :last_name_read, format: { with: VALID_NAME_READ_REGEX }
+    validates :first_name_read, format: { with: VALID_NAME_READ_REGEX }
+    validates :birth_date
+    validates :password_confirmation
+  end
   validates :email, uniqueness: true
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-  validates :password_confirmation, presence: true
 end
