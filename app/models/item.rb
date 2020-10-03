b4b89f0a4c9a6bbcd6209a72102f870063affa60
maekawa <kazumaekawa7218@gmail.com>
2020-10-03 16:17:41 +0900

    バリデーションとアソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+  with_options  presence: true do
+    validates :name
+    validates :text
+    validates :category_id
+    validates :state_id
+    validates :burden_id
+    validates :area_id
+    validates :day_id
+    validates :price
+    validates :image
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
   VALID_NAME_READ_REGEX = /\A[ァ-ヶー－]+\z/.freeze
