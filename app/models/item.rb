45609b490ca4c7aed96241b0955ca482fb3fa4c7
maekawa <kazumaekawa7218@gmail.com>
2020-10-03 17:02:32 +0900

    activ_hashのアソシエーション追加とAreaモデル修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash (:category, :state, :burden, :area, :day)
   with_options  presence: true do
     validates :name
     validates :text
