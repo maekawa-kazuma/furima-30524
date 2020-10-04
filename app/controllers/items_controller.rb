6290f0e677333778a92b2696c704f4e61f95d589
maekawa <kazumaekawa7218@gmail.com>
2020-10-04 18:18:00 +0900

    重複コードの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :new]
+  before_action :set_item, only: [:edit, :update, :show]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -18,16 +19,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path if user_signed_in? && current_user.id != @item.user_id
+    if user_signed_in? && current_user.id != @item.user_id
+      redirect_to root_path 
+    end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -39,5 +39,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :text, :category_id, :state_id, :burden_id, :area_id, :day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
