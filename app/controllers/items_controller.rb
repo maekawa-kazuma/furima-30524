88f62fe0529dc6e9af01f18943a23a78860ea75a
maekawa <kazumaekawa7218@gmail.com>
2020-10-04 20:23:26 +0900

    destroyアクションのエラーハンドリングとbefor_actionの適応

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :new]
-  before_action :set_item, only: [:edit, :update, :show]
+  before_action :set_item, only: [:edit, :update, :show, :destroy]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -34,9 +34,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
