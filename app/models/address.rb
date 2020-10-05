2be862794cade080b1a8aafe33bb3af28e9589a0
maekawa <kazumaekawa7218@gmail.com>
2020-10-06 08:57:24 +0900

    addressモデルにバリデーションを設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,14 @@
 class Address < ApplicationRecord
   belongs_to :purchase
+
+  VALID_POSTAL_CODE_REGEX = /\A\d{3}[-]\d{4}\z/
+  VALID_PHONE_NUMBER_REGEX = /\A\d{10,11}\z/
+  
+  with_options presence: true do
+    validates :postal_code, format: {with: VALID_POSTAL_CODE_REGEX }
+    validates :prefecture_id
+    validates :city
+    validates :house_number
+    validates :phone_number, format: {with: VALID_PHONE_NUMBER_REGEX}
+  end
 end
