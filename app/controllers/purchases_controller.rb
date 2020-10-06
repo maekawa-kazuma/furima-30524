1d03a847928102c32c6a5576458078803db6ddea
maekawa <kazumaekawa7218@gmail.com>
2020-10-06 12:31:07 +0900

    purchases_controllerのアクション定義

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,22 @@
 class PurchasesController < ApplicationController
   
   def index
+    @purchase_address = PurchaseAddress.new
   end
 
   def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
   end
 
+  private
+
+  def purchase_params
+    params.require(:purchase_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number)
+  end
 end
