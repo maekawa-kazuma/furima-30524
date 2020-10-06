163ef08e2be6fc69b79ecf2beadf64c7868e0400
maekawa <kazumaekawa7218@gmail.com>
2020-10-06 21:36:07 +0900

    モデルattr_accessorの追加

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token, :item_id, :user_id
 
   VALID_POSTAL_CODE_REGEX = /\A\d{3}[-]\d{4}\z/
   VALID_PHONE_NUMBER_REGEX = /\A\d{10,11}\z/
@@ -13,8 +13,7 @@ class PurchaseAddress
     validates :phone_number, format: {with: VALID_PHONE_NUMBER_REGEX}
   end
   def save
-    user = User.all
-    purchase = Purchase.create(user_id: currrent_user.id, item_id: params[item_id])
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
