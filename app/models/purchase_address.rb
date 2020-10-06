6a0d715de8a5b1b933bb49f3870eb80d1bf0cb76
maekawa <kazumaekawa7218@gmail.com>
2020-10-06 12:12:59 +0900

    データをテーブルに保存する処理とバリデーションの移動

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,4 +1,17 @@
 class PurchaseAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+
+  with_options presence: true do
+    validates :postal_code, format: {with: VALID_POSTAL_CODE_REGEX }
+    validates :prefecture_id
+    validates :city
+    validates :house_number
+    validates :phone_number, format: {with: VALID_PHONE_NUMBER_REGEX}
+  end
+  def save
+    user = User.all
+    purchase = Purchase.create(user_id: currrent_user.id, item_id: params[item_id])
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+  end
 end
