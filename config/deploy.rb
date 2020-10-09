21573dffd645c4f42a7a4aca92392c4900a34fdf
maekawa <kazumaekawa7218@gmail.com>
2020-10-09 14:44:22 +0900

    githubのurl指定

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.14.1'
 set :application, 'furima-30524'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'maekawa-kazuma/furima-30524.git'
+set :repo_url,  'git@github.com:maekawa-kazuma/furima-30524.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
